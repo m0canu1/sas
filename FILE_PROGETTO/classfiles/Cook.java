dea62a77851daa488c2063ec1522f9489d5c8792
m0canu1 <alexandrumocanu9@gmail.com>
2019-09-30 11:27:39 +0200

    aaaa

--- FILE_PROGETTO/classfiles/Cook.java
+++
@@ -1,9 +0,0 @@
-public class Cook {
-
-    private String name;
-    
-    public Cook(String n) {this.name = n;}
-
-    public String getName() {return this.name;}
-
-}
--- FILE_PROGETTO/classfiles/Recipe.java
+++
@@ -1,9 +0,0 @@
-public class Recipe{
-    
-    private String name;
-
-    public Recipe(String n) {this.name = n;}
-
-    public String getName() {return this.name;}
-
-}
